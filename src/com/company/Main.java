6398773c120ef0cbcd73ccbc5b11160d20e05e37
faithckorir <faithckorir@gmail.com>
2020-08-05 17:41:00 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+   static  int factorial(int n){
+       if (n==1){
+           return 1;
+       }else{
+           return n*factorial(n-1);
+       }
+   }
 
     public static void main(String[] args) {
-	// create a hashMap
-        Map<String, Boolean> myShoppingList= new HashMap<>();
-        //initialize the hashmap with values
-        myShoppingList.put("sugar",true);
-        myShoppingList.put("soap",true);
-        myShoppingList.put("oil",true);
-        myShoppingList.put("salt",false);
+        System.out.println(factorial(5));
 
-
-
- //print out keys and their values
-        System.out.println(myShoppingList.toString());
-
-
-        //check if the map is empty
-        System.out.println(myShoppingList.isEmpty());
-        //remove an item from the shopping list
-        myShoppingList.remove("oil");
-        //check the remaining elements
-        System.out.println(myShoppingList.toString());
-
-        //check the size of the shoppping list
-        System.out.println(myShoppingList.size());
-
-        //clear the shopping list
-        //myShoppingList.clear();
     }
 }
